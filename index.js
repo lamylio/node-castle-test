91437db06423f305f8911709eb911981957d6d14
Lioche <lionel.lamy@student.uclouvain.be>
2020-05-03 22:34:33 +0200

    Correction

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 const proxy = http.createServer((req, res) => {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-        if(err) res.end('ERROR');
+    if(req.url == "/"){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) res.end('ERROR');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        });
+    }else{
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content, 'utf-8');
-    });
+        res.end(content, req.url);
+    }
+    
 });
 
 proxy.listen(5050);

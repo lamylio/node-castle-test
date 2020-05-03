145c4e9ae00ff1ef0d37f19f83d1933f3be5897f
Lioche <lionel.lamy@student.uclouvain.be>
2020-05-03 23:15:51 +0200

    This time it the good one.

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 const proxy = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-    let ext = path.extname(filePath);
+    let filePath = path.join(__dirname, 'public');
+    let ext = path.extname(req.url);
+    let contentType;
+    
+    switch (ext) {
+        case '.css':
+            contentType = "text/css";
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.js':
+            contentType = "text/javascript";
+            break;
+        case '.json':
+            contentType = "application/json";
+            break;
+        case '.ico':
+            contentType = "image/x-icon";
+            break;
+        default:
+            contentType = "text/html";
+            break;
+    }
 
-    let contentType = 'text/html';
+    filePath = path.join(filePath, req.url === '/' ? 'index.html' : (req.url + (ext == "" ? ".html" : "")));
+
+    console.log(filePath);
 
     fs.readFile(filePath, (err, content) => {
         if(err){

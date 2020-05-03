6c22b27155bc7f7214110d67085c8aff7692292c
Lioche <lionel.lamy@student.uclouvain.be>
2020-05-03 22:43:10 +0200

    Last try

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const fs = require('fs');
 const path = require('path');
 
 const proxy = http.createServer((req, res) => {
-    if(req.url == "/"){
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content, "Hello");
-    }    
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let ext = path.extname(filePath);
+
+    let contentType = 'text/html';
+
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT'){
+                res.end('NOT FOUND');
+            }else{
+                res.end('SERVER ERROR');
+            }
+        }
+
+        res.writeHead(200, {'Content-Type': contentType });
+        res.end(content, 'utf8');
+    });
 });
 
-proxy.listen(80);
+const PORT = process.env.PORT || 5000;
+proxy.listen(PORT, () => console.log(`Server running on ${PORT}`));

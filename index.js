dfb3778ac1611c00106453a25bba89e874480e57
Lioche <lionel.lamy@student.uclouvain.be>
2020-05-03 22:35:51 +0200

    Correction

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const path = require('path');
 
 const proxy = http.createServer((req, res) => {
     if(req.url == "/"){
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) res.end('ERROR');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf-8');
-        });
-    }else{
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content, req.url);
-    }
-    
+        res.end(content, "Hello");
+    }    
 });
 
-proxy.listen(5050);
+proxy.listen(80);
